a5f5945a7823e0d75cc477e04f8c3064ff8fe10b
horhey.ortega <horhey.ortega@gmail.com>
2017-07-01 00:51:50 -0400

    Optimization and unification changes

---
+++ RefreshRecentlyAddedService/RefreshRecentlyAdded.Lib/RefreshRecentlyAddedArgs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefreshRecentlyAdded.Lib
+{
+    public class RefreshRecentlyAddedArgs
+    {
+        public string ValidMovieExtensions { get; set; }
+
+        public bool Debug
+        {
+            get;
+            set;
+        }
+
+        public int DebugWaitTime
+        {
+            get;
+            set;
+        }
+
+        public int RandomPlaylistRefreshTimeIntervalInDays { get; set; }
+
+        public int RefreshTimeIntervalInDays { get; set; }
+
+        public int NumberOfRandomFiles { get; set; }
+
+        public string RecentlyAddedLocation { get; set; }
+
+        public string ScanLocation { get; set; }
+
+        public string RandomPlaylistLocation { get; set; }
+
+        public string DatabaseLocation { get; set; }
+
+        public int RandomFilePlayCountThresholdUntilDelete { get; set; }
+
+        public int MinimumPlayCount { get; set; }
+    }
+}
--- RefreshRecentlyAddedService/RefreshRecentlyAdded.Lib/RefreshRecentlyAddedRunner.cs
+++ RefreshRecentlyAddedService/RefreshRecentlyAdded.Lib/RefreshRecentlyAddedRunner.cs
@@ -1,80 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Threading;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using RefreshRecentlyAddedDataStore;
 using RefreshRecentlyAddedLogger;
-using RefreshRecentlyAddedService;
 
 namespace RefreshRecentlyAdded.Lib
 {
     public class RefreshRecentlyAddedRunner
     {
-
         private readonly Logger _logger = new Logger();
 
-        public string ValidMovieExtensions { get; set; }
-
-        public bool Debug
-        {
-            get;
-            set;
-        }
-
-        public int DebugWaitTime
-        {
-            get;
-            set;
-        }
-
-        public bool RunRecentlyAddedRoutine
-        {
-            get;
-            set;
-        }
-
-        public bool RunRandomPlaylistRoutine
-        {
-            get;
-            set;
-        }
-
-        public string RandomOrgEndpoint
-        {
-            get;
-            set;
-        }
-
-        public string ApiKey
-        {
-            get;
-            set;
-        }
-
-        public int RandomPlaylistRefreshTimeIntervalInDays { get; set; }
-
-        public int RefreshTimeIntervalInDays { get; set; }
-
-        public int RefreshRateInSeconds { get; set; }
-
-        public int NumberOfRandomFiles { get; set; }
-
-        public string RecentlyAddedLocation { get; set; }
-
-        public string ScanLocation { get; set; }
-
-        public string RandomPlaylistLocation { get; set; }
+        public RefreshRecentlyAddedArgs Args { get; set; }
 
         public RefreshRecentlyAddedDataStoreProvider DataStoreProvider { get; set; }
-
-        private Thread RefreshThread;
 
         [DllImport("kernel32.dll")]
         static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, SymbolicLink dwFlags);
@@ -85,106 +26,69 @@ namespace RefreshRecentlyAdded.Lib
             Directory = 1
         }
 
-        public RefreshRecentlyAddedRunner()
+        protected void Create(RefreshRecentlyAddedArgs args)
         {
-
+            Args = args;
+            DataStoreProvider = new RefreshRecentlyAddedDataStoreProvider(args.DatabaseLocation,
+                args.RandomFilePlayCountThresholdUntilDelete, args.MinimumPlayCount);
         }
 
-        public RefreshRecentlyAddedRunner(int refreshTimeIntervalInDays,
-                                            int refreshRateInSeconds,
-                                            int randomPlaylistRefreshTimeIntervalInDays,
-                                            int numberOfRandomFiles,
-                                            string recentlyAddedLocation,
-                                            string scanLocation,
-                                            string randomPlaylistLocation,
-                                            string validMovieExtensions,
-                                            bool debug,
-                                            int debugWaitTime,
-                                            bool runRecentlyAddedRoutine,
-                                            bool runRandomPlaylistRoutine,
-                                            string randomOrgEndpoint,
-                                            string apiKey,
-                                            int minimumPlayCount,
-                                            int randomPlayCountUntilDelete,
-                                            string databaseLocation)
+        public RefreshRecentlyAddedRunner(RefreshRecentlyAddedArgs args)
         {
-            RandomPlaylistRefreshTimeIntervalInDays = randomPlaylistRefreshTimeIntervalInDays;
-            RefreshTimeIntervalInDays = refreshTimeIntervalInDays;
-            RefreshRateInSeconds = refreshRateInSeconds;
-            RecentlyAddedLocation = recentlyAddedLocation;
-            ScanLocation = scanLocation;
-            RandomPlaylistLocation = randomPlaylistLocation;
-            NumberOfRandomFiles = numberOfRandomFiles;
-            ValidMovieExtensions = validMovieExtensions;
-            Debug = debug;
-            DebugWaitTime = debugWaitTime;
-            RunRecentlyAddedRoutine = runRecentlyAddedRoutine;
-            RunRandomPlaylistRoutine = runRandomPlaylistRoutine;
-            RandomOrgEndpoint = randomOrgEndpoint;
-            ApiKey = apiKey;
-            DataStoreProvider = new RefreshRecentlyAddedDataStoreProvider(databaseLocation, randomPlayCountUntilDelete, minimumPlayCount);
-
-            RefreshThread = new Thread(RefreshFiles);
-        }
-
-        public void StopRefreshing()
-        {
-            if (RefreshThread.ThreadState != System.Threading.ThreadState.AbortRequested &&
-                RefreshThread.ThreadState != System.Threading.ThreadState.StopRequested &&
-                RefreshThread.ThreadState != System.Threading.ThreadState.SuspendRequested &&
-                RefreshThread.ThreadState != System.Threading.ThreadState.Stopped &&
-                RefreshThread.ThreadState != System.Threading.ThreadState.Unstarted)
-            {
-                RefreshThread.Abort();
-            }
+            Create(args);
         }
 
         public void RefreshFiles()
         {
-            if (Debug)
+            if (Args.Debug)
             {
-                Thread.Sleep(DebugWaitTime);
+                Thread.Sleep(Args.DebugWaitTime);
             }
 
-            if (RunRandomPlaylistRoutine)
-            {
-                CreateRandomFilesInLocationFunction(ScanLocation,
-                    RandomPlaylistLocation,
-                    RandomPlaylistRefreshTimeIntervalInDays);
+            List<FileInfo> filesInScanLocation = GetScannableFiles(Args.ScanLocation).ToList();
 
-                DataStoreProvider.PersistDatabase();
+            if(!Directory.Exists(Args.RandomPlaylistLocation))
+            {
+                Directory.CreateDirectory(Args.RandomPlaylistLocation);
             }
 
-            if (RunRecentlyAddedRoutine)
+            if (!Directory.Exists(Args.RecentlyAddedLocation))
             {
-                RefreshLocationFunction(ScanLocation,
-                    RecentlyAddedLocation,
-                    RefreshTimeIntervalInDays);
+                Directory.CreateDirectory(Args.RecentlyAddedLocation);
             }
+
+            CleanDirectory(Args.RandomPlaylistLocation);
+
+            CleanDirectory(Args.RecentlyAddedLocation);
+
+            RefreshRandomFiles(filesInScanLocation,
+                Args.RandomPlaylistLocation,
+                Args.RandomPlaylistRefreshTimeIntervalInDays);
+
+            RefreshRecentlyAddedFiles(filesInScanLocation,
+                Args.RecentlyAddedLocation,
+                Args.RefreshTimeIntervalInDays);
+
+            DataStoreProvider.PersistDatabase();
         }
 
-        private bool FileIsVideoFile(string Extension)
+        private bool IsFileAllowed(string fileName)
         {
-            bool result = GetValidMovieFileExtensions().Contains(Extension);
-
-            return result;
-        }
-
-        private List<string> GetFileNames(string[] Files)
-        {
-            List<string> FileNames = new List<string>();
-            foreach (string file in Files)
+            foreach (string validMovieFileExtension in GetValidMovieFileExtensions())
             {
-                FileInfo Info = new FileInfo(file);
-                FileNames.Add(Info.Name);
+                if (fileName.ToLower().EndsWith(validMovieFileExtension.ToLower()))
+                {
+                    return true;
+                }
             }
-            return FileNames;
+
+            return false;
         }
 
         private List<string> GetValidMovieFileExtensions()
         {
             List<string> validMovieFileExtensions = new List<string>();
-            string extensionsFromConfig = ValidMovieExtensions;
+            string extensionsFromConfig = Args.ValidMovieExtensions;
             string[] extensionsSplit = extensionsFromConfig.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
             foreach (string s in extensionsSplit)
             {
@@ -193,21 +97,13 @@ namespace RefreshRecentlyAdded.Lib
             return validMovieFileExtensions;
         }
 
-        public void CleanUpLocationFunction(string locationToCleanUp)
+        public void CleanDirectory(string directory)
         {
             try
             {
-                if (!Directory.Exists(locationToCleanUp))
-                {
-                    Directory.CreateDirectory(locationToCleanUp);
-                }
+                List<FileInfo> files = GetFilesInLocation(directory, true).ToList();
 
-                string[] files = Directory.GetFiles(locationToCleanUp, "*.*", SearchOption.AllDirectories);
-
-                foreach (string currentFile in files)
-                {
-                    File.Delete(currentFile);
-                }
+                files.AsParallel().ForAll(dd => File.Delete(dd.FullName));
             }
             catch (Exception e)
             {
@@ -215,54 +111,36 @@ namespace RefreshRecentlyAdded.Lib
             }
         }
 
-        public void RefreshLocationFunction(string locationOfSourceFiles,
-                                                 string locationToCopyTo,
-                                                 int refreshTimeInterval)
+        public void RefreshRecentlyAddedFiles(IEnumerable<FileInfo> fileInfos,
+            string locationToCopyTo,
+            int refreshTimeInterval)
         {
             try
             {
+                List<FileInfo> filesInScanLocationList = fileInfos.OrderByDescending(dd => dd.CreationTime).ToList();
 
-                CleanUpLocationFunction(locationToCopyTo);
-
-                if (!Directory.Exists(locationToCopyTo))
-                {
-                    Directory.CreateDirectory(locationToCopyTo);
-                }
-
-                string[] filesInScanLocation = Directory.GetFiles(locationOfSourceFiles, "*.*", SearchOption.AllDirectories);
-                string[] filesInDestination = Directory.GetFiles(locationToCopyTo, "*.*", SearchOption.AllDirectories);
-
-                List<String> filesInScanLocationList = filesInScanLocation.ToList();
-                List<String> filesInDestinationList = GetFileNames(filesInDestination);
-
-                filesInScanLocationList.RemoveAll(ss => ss.Contains(RandomPlaylistLocation));
-                filesInScanLocationList.RemoveAll(ss => ss.Contains(RecentlyAddedLocation));
-
-                List<FileInfo> fileInfos = filesInScanLocationList.Select(dd => new FileInfo(dd)).OrderByDescending(dd => dd.CreationTime).ToList();
+                List<FileInfo> filesInDestination = GetFilesInLocation(locationToCopyTo).ToList();
 
                 int count = 1;
 
-                foreach (FileInfo infoAboutFile in fileInfos)
+                foreach (FileInfo infoAboutFile in filesInScanLocationList)
                 {
-                    if (!infoAboutFile.Name.ToLower().Contains(".DS_Store".ToLower()) &&
-                        FileIsVideoFile(infoAboutFile.Extension))
+                    if (filesInDestination.FirstOrDefault(dd => dd.FullName.Contains(infoAboutFile.Name)) != null)
                     {
-                        if (filesInDestinationList.Contains(infoAboutFile.Name))
-                        {
-                            continue;
-                        }
+                        continue;
+                    }
 
-                        if (infoAboutFile.CreationTime >= DateTime.Today.AddDays(refreshTimeInterval * -1) &&
-                            infoAboutFile.CreationTime <= DateTime.Today.AddDays(1).AddMilliseconds(-1))
-                        {
-                            string originalPath = infoAboutFile.FullName;
+                    if (infoAboutFile.CreationTime >= DateTime.Today.AddDays(refreshTimeInterval * -1) &&
+                        infoAboutFile.CreationTime <= DateTime.Today.AddDays(1).AddMilliseconds(-1))
+                    {
+                        string originalPath = infoAboutFile.FullName;
 
-                            string newPath = Path.Combine(locationToCopyTo, string.Format("{0}-{1}", count, infoAboutFile.Name));
+                        string newPath = Path.Combine(locationToCopyTo,
+                            string.Format("{0}-{1}", count, infoAboutFile.Name));
 
-                            CreateSymbolicLink(newPath, originalPath, SymbolicLink.File);
+                        CreateSymbolicLink(newPath, originalPath, SymbolicLink.File);
 
-                            count++;
-                        }
+                        count++;
                     }
                 }
             }
@@ -308,6 +186,8 @@ namespace RefreshRecentlyAdded.Lib
 
                     DataStoreProvider.IncreasePlayCount(info.FullName);
 
+                    files.RemoveAll(dd => dd.FullName.Equals(info.FullName));
+
                     readyToReturn = fileInfosToReturn.Count == numberOfRandomFiles;
 
                     if (readyToReturn)
@@ -317,35 +197,45 @@ namespace RefreshRecentlyAdded.Lib
                 }
 
                 iterationCount++;
-
-                Thread.Sleep(50);
             }
 
             return fileInfosToReturn;
         }
 
-        public void CreateRandomFilesInLocationFunction(string locationOfSourceFiles,
-                                                        string locationToCopyTo,
-                                                        int refreshTimeInterval)
+        public IEnumerable<FileInfo> GetFilesInLocation(string location, bool ignoreRestrictions = false)
+        {
+            List<FileInfo> files =
+                Directory.EnumerateFiles(location, "*.*", SearchOption.AllDirectories)
+                    .Where(dd => ignoreRestrictions || IsFileAllowed(dd))
+                    .Select(dd => new FileInfo(dd))
+                    .OrderBy(dd => dd.Name)
+                    .ToList();
+
+            return files;
+        }
+
+        public IEnumerable<FileInfo> GetScannableFiles(string scanLocation)
+        {
+            List<FileInfo> filesInScanLocationList = GetFilesInLocation(scanLocation).ToList();
+
+            filesInScanLocationList.RemoveAll(
+                ss => ss.DirectoryName != null && ss.DirectoryName.Contains(Args.RandomPlaylistLocation));
+
+            filesInScanLocationList.RemoveAll(
+                ss => ss.DirectoryName != null && ss.DirectoryName.Contains(Args.RecentlyAddedLocation));
+
+            return filesInScanLocationList;
+        }
+
+        public void RefreshRandomFiles(IEnumerable<FileInfo> filesInScanLocation,
+                                        string locationToCopyTo,
+                                        int refreshTimeInterval)
         {
             try
             {
-                CleanUpLocationFunction(locationToCopyTo);
+                List<FileInfo> filesInScanLocationList = filesInScanLocation.ToList();
 
-                if (!Directory.Exists(locationToCopyTo))
-                {
-                    Directory.CreateDirectory(locationToCopyTo);
-                }
-
-                List<FileInfo> filesInScanLocationList = Directory.GetFiles(locationOfSourceFiles, "*.*", SearchOption.AllDirectories).Select(dd => new FileInfo(dd)).OrderBy(dd => dd.Name).ToList();
-
-                filesInScanLocationList.RemoveAll(ss => ss.DirectoryName.Contains(RandomPlaylistLocation));
-
-                filesInScanLocationList.RemoveAll(ss => ss.DirectoryName.Contains(RecentlyAddedLocation));
-
-                filesInScanLocationList.RemoveAll(ss => !FileIsVideoFile(ss.Extension));
-
-                List<FileInfo> randomFiles = GetRandomFiles(NumberOfRandomFiles, filesInScanLocationList).ToList();
+                List<FileInfo> randomFiles = GetRandomFiles(Args.NumberOfRandomFiles, filesInScanLocationList).ToList();
 
                 int count = 1;
 
@@ -364,8 +254,6 @@ namespace RefreshRecentlyAdded.Lib
             {
                 _logger.LogException(e);
             }
-
         }
-
     }
 }
