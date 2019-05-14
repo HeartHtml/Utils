342e265ab5c68034676c3ab7a882bc71e1d4bc07
horhey.ortega <horhey.ortega@gmail.com>
2019-05-13 21:29:43 -0400

    IntegrityBuilders.Site

---
+++ IntegrityBuilders.Site/IntegrityBuilders.Site/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntegrityBuilders.Site.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Content
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
